5ac322fd770093a34d3a017963783cc909980402
Czipperz <czipperz@gmail.com>
2016-06-24 18:38:36 -0700

    Add MPL notice

--- lib.hh
+++ lib.hh
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #ifndef HEADER_GUARD_VICK_WRITE_FILE_H
 #define HEADER_GUARD_VICK_WRITE_FILE_H
 
