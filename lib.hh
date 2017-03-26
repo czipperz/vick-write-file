11f3c10889de293a5c70fe726607d1be35ac6b2d
Czipperz <czipperz@gmail.com>
2017-03-25 22:10:12 -0500

    Don't use the redundant boost::optional wrapper around changes

--- lib.hh
+++ lib.hh
@@ -22,20 +22,20 @@ namespace write_file {
  *
  * Does not edit the contents.
  */
-inline boost::optional<std::shared_ptr<change> >
+inline std::shared_ptr<change>
 write(contents&, boost::optional<int> = boost::none);
 
 /*!
  * \brief Do `write()` in a new thread.
  */
-inline boost::optional<std::shared_ptr<change> >
+inline std::shared_ptr<change>
 write_async(contents&, boost::optional<int> = boost::none);
 
 /*!
  * \brief Writes the file synchronously if given a prefix arg > 0,
  * else writes asynchronously.
  */
-inline boost::optional<std::shared_ptr<change> >
+inline std::shared_ptr<change>
 write_maybe_async(contents&, boost::optional<int> = boost::none);
 
 /*!
@@ -45,19 +45,19 @@ write_maybe_async(contents&, boost::optional<int> = boost::none);
  */
 int write_to(const contents&, const char* file_name);
 
-inline boost::optional<std::shared_ptr<change> >
+inline std::shared_ptr<change>
 write(contents& c, boost::optional<int>) {
     write_to(c, c.file_name.c_str());
-    return boost::none;
+    return nullptr;
 }
 
-inline boost::optional<std::shared_ptr<change> >
+inline std::shared_ptr<change>
 write_async(contents& c, boost::optional<int>) {
     std::thread(write_to, std::ref(c), c.file_name.c_str()).detach();
-    return boost::none;
+    return nullptr;
 }
 
-inline boost::optional<std::shared_ptr<change> >
+inline std::shared_ptr<change>
 write_maybe_async(contents& c, boost::optional<int> op) {
     if (op && *op > 0) {
         return write(c, std::move(op));
